ae68d97ad20059efa8c7e01d0bdeb9c836b98665
MyWaveTraningMail <MyWaveTraning@mail.ru>
2025-11-27 21:50:20 +0000

    📋 Добавлены скрипты для загрузки FAQ в Google Sheets (10 вопросов-ответов)

---
+++ scripts/__init__.py
@@ -0,0 +1,7 @@
+"""
+Вспомогательные скрипты для инициализации и управления данными бота Pilates Reformer.
+
+Доступные скрипты:
+- populate_faq.py: Заполнение FAQ в Google Sheets
+- populate_faq_local.py: Заполнение FAQ в локальной БД (SQLite)
+"""
